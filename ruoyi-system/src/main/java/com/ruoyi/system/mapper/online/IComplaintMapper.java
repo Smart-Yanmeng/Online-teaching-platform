4b9d2406280a0b88253de0b67925c35eba67b819
York <York.Yanmeng@gmail.com>
2023-05-28 02:21:07 +0800

    2023/05/28

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/online/IComplaintMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/online/IComplaintMapper.java
@@ -2,7 +2,6 @@ package com.ruoyi.system.mapper.online;
 
 import com.ruoyi.system.domain.bo.complaint.ComplaintUpdateBo;
 import com.ruoyi.system.domain.po.BComplaintPo;
-import com.ruoyi.system.domain.dto.complaint.ComplaintUpdateDto;
 
 import java.util.List;
 
